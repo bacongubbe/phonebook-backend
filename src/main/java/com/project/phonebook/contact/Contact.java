145980e7fbc4af027a6e2cba1e6b6b5d791d5a6f
bacongubbe <berg.felix@hotmail.com>
2022-08-20 15:56:06 +0200

    fixing UUID in constructor. Enabling CORS

--- src/main/java/com/project/phonebook/contact/Contact.java
+++ src/main/java/com/project/phonebook/contact/Contact.java
@@ -1,6 +1,13 @@
 package com.project.phonebook.contact;
 
 import javax.validation.constraints.NotEmpty;
+import java.util.UUID;
 
 public record Contact(String id, @NotEmpty String name, String address, String phoneNumber) {
+    public Contact(String id, @NotEmpty String name, String address, String phoneNumber) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
 }
